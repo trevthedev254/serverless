64d46a1b2a89b6bd84056a933233678990876418
trevthedev254 <trvetech@gmail.com>
2022-10-13 19:02:03 +0300

    final

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'beu91cq67l';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-eszkjj97.us.auth0.com',
+  clientId: 'GxU3lhEgDQyoGEGcjVMDLSAcC2RAuo5I',
+  callbackUrl: 'http://localhost:3000/callback'
+};
